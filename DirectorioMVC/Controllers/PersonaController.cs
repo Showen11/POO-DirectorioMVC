8e4b5384c47dc77610ba2ef56d5fbd88bdc13bb2
Showen11 <owenamadoe@gmail.com>
2025-09-08 22:06:02 -0600

    D

---
+++ DirectorioMVC/Controllers/PersonaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirectorioMVC.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
